5f8dfb51fe16cf951a21c0dc324561e41d090539
afm97 <arthurfranca97@gmail.com>
2023-10-09 13:38:17 -0300

    Chamada de funcoes e insercao dos valores de entrada

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include "function.h"
+#include "increment.h"
+#include "dataFormat.h"
+#include "printArray.h"
+#include "eulerMethod.h"
 
 int main()
 {
+    boundaryConditions problem = {.timeStart = 0.0,
+                                  .timeEnd = 2.0,
+                                  .NumberOfSteps = 4,
+                                  .initialCondition = 0.5};
+
+    double step = increment(problem.timeStart, problem.timeEnd, problem.NumberOfSteps);
+
+    double *result = malloc(sizeof(double) * (problem.NumberOfSteps + 1));
+    result[0] = problem.initialCondition;
+
+    eulerMethod(result, problem.timeStart, step, problem.NumberOfSteps + 1);
+
+    printArray(problem.NumberOfSteps + 1, result);
+    free(result);
+    result = NULL;
 }
