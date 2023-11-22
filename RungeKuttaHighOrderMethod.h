ebb59abc3061dc70ec65efdd80ee42edb134a83d
afm97 <arthurfranca97@gmail.com>
2023-11-22 10:59:46 -0300

    Corrigido bug de tempo calculado de maneira errônea

--- RungeKuttaHighOrderMethod.h
+++ RungeKuttaHighOrderMethod.h
@@ -2,21 +2,24 @@ double func(double t, double y);
 
 double rungeKuttaHighOrderMethod(unsigned int numberOfSteps, double timeStart, double step, double solution[numberOfSteps + 1])
 {
-    double K_1, K_2, K_3, K_4, K_5, K_6, currentTime;
-    unsigned int iterator = 0;
+    double K_1, K_2, K_3, K_4, K_5, K_6, currentTime, y0;
+    unsigned int iterator = 1;
 
     do
     {
         currentTime = timeStart + iterator * step;
 
-        K_1 = func(currentTime, solution[iterator]);
-        K_2 = func(currentTime + step / 4, solution[iterator] + K_1 * step / 4);
-        K_3 = func(currentTime + step / 4, solution[iterator] + (K_2 + K_1) * step / 8);
-        K_4 = func(currentTime + step / 2, solution[iterator] + (K_3 - K_2 / 2) * step);
-        K_5 = func(currentTime + 3 * step / 4, solution[iterator] + (9 * K_4 + 3 * K_1) * step / 16);
-        K_6 = func(currentTime + step / 2, solution[iterator] + (8 * K_5 - 12 * K_4 + 12 * K_3 + 2 * K_2 - 3 * K_1) * step / 7);
+        y0 = solution[iterator][1];
 
-        solution[iterator + 1] = solution[iterator] + (7 * K_1 + 32 * K_3 + 12 * K_4 + 32 * K_5 + 7 * K_6) * step / 90;
+        K_1 = func(currentTime, y0);
+        K_2 = func(currentTime + step / 4, y0 + K_1 * step / 4);
+        K_3 = func(currentTime + step / 4, y0 + (K_2 + K_1) * step / 8);
+        K_4 = func(currentTime + step / 2, y0 + (K_3 - K_2 / 2) * step);
+        K_5 = func(currentTime + 3 * step / 4, y0 + (9 * K_4 + 3 * K_1) * step / 16);
+        K_6 = func(currentTime + step / 2, y0 + (8 * K_5 - 12 * K_4 + 12 * K_3 + 2 * K_2 - 3 * K_1) * step / 7);
+
+        solution[iterator][0] = currentTime;
+        solution[iterator + 1] = y0 + (7 * K_1 + 32 * K_3 + 12 * K_4 + 32 * K_5 + 7 * K_6) * step / 90;
 
         iterator++;
 
--- RungeKuttaMethod.h
+++ RungeKuttaMethod.h
@@ -1,20 +1,26 @@
 double func(double t, double y);
 
-double rungeKuttaMethod(unsigned int numberOfSteps, double timeStart, double step, double solution[numberOfSteps + 1])
+double rungeKuttaMethod(unsigned int numberOfSteps, double timeStart, double step, double solution[numberOfSteps + 1][2])
 {
-    double K_1, K_2, K_3, K_4, currentTime;
-    unsigned int iterator = 0;
+    double K_1, K_2, K_3, K_4, currentTime, y0;
+
+    unsigned int iterator = 1;
 
     do
     {
         currentTime = timeStart + iterator * step;
 
-        K_1 = step * func(currentTime, solution[iterator]);
-        K_2 = step * func(currentTime + step / 2, solution[iterator] + K_1 / 2);
-        K_3 = step * func(currentTime + step / 2, solution[iterator] + K_2 / 2);
-        K_4 = step * func(currentTime + step, solution[iterator] + K_3);
+        y0 = solution[iterator][1];
 
-        solution[iterator + 1] = solution[iterator] + (K_1 + 2 * K_2 + 2 * K_3 + K_4) / 6;
+        K_1 = step * func(currentTime, y0);
+        K_2 = step * func(currentTime + step / 2, y0 + K_1 / 2);
+        K_3 = step * func(currentTime + step / 2, y0 + K_2 / 2);
+        K_4 = step * func(currentTime + step, y0 + K_3);
+
+        printf("tempo atual = %f\n", currentTime);
+
+        solution[iterator][0] = currentTime;
+        solution[iterator][1] = y0 + (K_1 + 2 * K_2 + 2 * K_3 + K_4) / 6;
 
         iterator++;
 
