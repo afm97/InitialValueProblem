7d7a4aa23d79546b3c46a33844c82f43f8df2a75
afm97 <arthurfranca97@gmail.com>
2023-11-22 11:11:37 -0300

    Correcao dos ultimos problemas dos Metodos RK

--- RungeKuttaMethod.h
+++ RungeKuttaMethod.h
@@ -2,7 +2,7 @@ double func(double t, double y);
 
 double rungeKuttaMethod(unsigned int numberOfSteps, double timeStart, double step, double solution[numberOfSteps + 1][2])
 {
-    double K_1, K_2, K_3, K_4, currentTime, y0;
+    double K_1, K_2, K_3, K_4, t0, y0, currentTime;
 
     unsigned int iterator = 1;
 
@@ -10,14 +10,13 @@ double rungeKuttaMethod(unsigned int numberOfSteps, double timeStart, double ste
     {
         currentTime = timeStart + iterator * step;
 
-        y0 = solution[iterator][1];
+        t0 = solution[iterator - 1][0];
+        y0 = solution[iterator - 1][1];
 
-        K_1 = step * func(currentTime, y0);
-        K_2 = step * func(currentTime + step / 2, y0 + K_1 / 2);
-        K_3 = step * func(currentTime + step / 2, y0 + K_2 / 2);
-        K_4 = step * func(currentTime + step, y0 + K_3);
-
-        printf("tempo atual = %f\n", currentTime);
+        K_1 = step * func(t0, y0);
+        K_2 = step * func(t0 + step / 2, y0 + K_1 / 2);
+        K_3 = step * func(t0 + step / 2, y0 + K_2 / 2);
+        K_4 = step * func(t0 + step, y0 + K_3);
 
         solution[iterator][0] = currentTime;
         solution[iterator][1] = y0 + (K_1 + 2 * K_2 + 2 * K_3 + K_4) / 6;
